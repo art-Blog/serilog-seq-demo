afc164846b7efb9981934a9134cd3d8dc98ee17b
art <partypeopleland@gmail.com>
2022-04-20 12:07:35 +0800

    fake db result

--- serilog-seq-demo.Adapter/MySql/AdminAdapter.cs
+++ serilog-seq-demo.Adapter/MySql/AdminAdapter.cs
@@ -9,21 +9,30 @@ public static class AdminAdapter
 {
     public static IEnumerable<AdminRecord> GetAdminsByAuthority(int? authority)
     {
-        var parameters = new MySqlParameter[]
-        {
-            new("@authority", MySqlDbType.Int16) { Value = authority },
-        };
+        // var parameters = new MySqlParameter[]
+        // {
+        //     new("@authority", MySqlDbType.Int16) { Value = authority },
+        // };
 
-        return MySqlProxy.QueryCollection<AdminRecord>(DBConfig.MyConnString, CommandType.Text, Command.Admin.GetAdminsByAuthority, parameters);
+        // return MySqlProxy.QueryCollection<AdminRecord>(DBConfig.MyConnString, CommandType.Text, Command.Admin.GetAdminsByAuthority, parameters);
+
+        var fake = new List<AdminRecord>()
+        {
+            new AdminRecord() { Account = "art", Authority = 0, Id = "art", Name = "art" },
+            new AdminRecord() { Account = "bob", Authority = 0, Id = "bob", Name = "bob" },
+        };
+        return fake;
     }
 
     public static AdminRecord? GetAdminById(string id)
     {
-        var parameters = new MySqlParameter[]
-        {
-            new("@id", MySqlDbType.VarChar) { Value = id },
-        };
+        // var parameters = new MySqlParameter[]
+        // {
+        //     new("@id", MySqlDbType.VarChar) { Value = id },
+        // };
+        //
+        // return MySqlProxy.Query<AdminRecord>(DBConfig.MyConnString, CommandType.Text, Command.Admin.GetAdminById, parameters);
+        return new AdminRecord() { Account = "art", Authority = 0, Id = "art", Name = "art" };
 
-        return MySqlProxy.Query<AdminRecord>(DBConfig.MyConnString, CommandType.Text, Command.Admin.GetAdminById, parameters);
     }
 }
